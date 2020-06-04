0c6fb54fa31ecfbd2dd8c9a2230c3ceebb1841a5
menglei1616 <2227025182@qq.com>
2020-06-04 17:18:45 +0800

    main

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println(new BigDecimal(0));
+        System.out.println(new BigDecimal(1));
     }
 }
