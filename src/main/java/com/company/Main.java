c43e30e82699efdb563b3a680be53ff9175179cd
mengl16 <2604245295@qq.com>
2020-06-10 09:32:20 +0800

    update04

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println(new BigDecimal(0));
         System.out.println(new BigDecimal(1));
         System.out.println(new BigDecimal(2));
+        System.out.println(new BigDecimal(3));
     }
 }
